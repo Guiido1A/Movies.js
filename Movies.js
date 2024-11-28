d387396a801b04d0562c5f052d51b70b30da4dd9
Guiido1A <guilherme.riter.domingues@escola.pr.gov.br>
2024-11-28 17:43:33 -0300

    Create Movies.js

---
+++ Movies.js
@@ -0,0 +1,320 @@
+========COMEDIA
+
+title:Todo Poderoso,
+genre:comedia,
+ageRating: livre,
+synopsis:Bruce Nolan é um jornalista que tem a vida perfeita. Quando as coisas começam a dar errado, ele questiona Deus e a forma como ele comanda a Terra. O Todo Poderoso dá ao rapaz todos seus poderes, mas Bruce percebe como é difícil ser onipresente,
+trailer:https://youtu.be/OOVmS_Z3ZZs?si=3MYWqpZoDsVAQTzL
+
+},
+
+{
+title:ParaNorman,
+genre:comedia,
+ageRating: 10,
+synopsis:O jovem Norman Babcock consegue falar com os mortos e prefere conviver com eles a conviver com os vivos. Seu tio Prenderghast se comunica com ele para dizer que a maldição de uma bruxa centenária vai se realizar, e apenas Norman pode detê-la,
+trailer:https://youtu.be/F2Bg0RLc2mg?si=Vb_Q-aUk3PVE7sBh
+},
+
+{
+title:Vizinhos,
+genre:comedia,
+ageRating: 12,
+synopsis:Walter tem um colapso nervoso e decide trocar a cidade grande pelo campo. Mas seus vizinhos barulhentos acabam com seu sonho de paz e tranquilidade,
+trailer:https://youtu.be/IaV-OO-Mwx0?si=Z9Qv6EcEuJdF-9oa
+},
+
+{
+title:Superbad - É Hoje
+genre:comedia,
+ageRating: 14,
+synopsis:Os adolescentes Seth e Evan têm grandes esperanças para uma festa de formatura. Eles pretendem beber e conquistar as garotas para que eles possam se tornar parte da turma mais popular da escola, mas a ansiedade de separação e dois policiais entediados complicam missão dos amigos,
+trailer:https://youtu.be/LvKvus3vCEY?si=DI0KRURk7dJ42lAQ
+},
+
+{
+title:Todo Mundo em Pânico 2,
+genre:comedia,
+ageRating: 16,
+synopsis:Cindy, Ray, Shorty e Brenda tentam esquecer os incidentes que ocorreram no último Halloween. Entretanto, devem lidar com um projeto escolar que inclui uma visita à Casa do Inferno, um local onde aconteceu uma sessão frustrada de exorcismo no passado,
+trailer:https://youtu.be/7dEorT3ZpRk?si=Yz6BWdiFl20YMXHT
+
+========FICÇÃO CIENTÍFICA
+
+title:A Família Mitchell e a Revolta das Máquinas,
+genre:ficção científica,
+ageRating: livre,
+synopsis:Uma adolescente embarca em uma viagem com seus orgulhosos pais, irmão mais novo e cachorro amado para começar seu primeiro ano na faculdade. Mas seus planos de se unir como uma família logo são interrompidos quando os dispositivos eletrônicos do mundo se revoltam e rapidamente organizam um apocalipse robô. Agora, só a estranha família Mitchell pode salvar a humanidade.,
+trailer:https://youtu.be/_8kRAlYnmvU?si=-RCDsPOciBvwvxVD
+
+},
+
+{
+title:Pixels,
+genre:ficção científica,
+ageRating: 10,
+synopsis: Em busca de contato com seres extraterrestres, a humanidade enviou imagens e sons variados sobre sua própria cultura nos mais diversos satélites lançados ao espaço. Após encontrar um desses registros, uma raça alienígena resolve criar monstros digitais inspirados em videogames clássicos dos anos 1980 e tentar conquistar a Terra. Para combatê-los, um grupo de especialistas em jogos da época é convocado e encarregado de derrotar os invasores, salvando assim o planeta.,
+trailer:https://youtu.be/KRNa8PQF8sg?si=ylrerpGiyiFxlmMu
+},
+
+{
+title:O Astronauta,
+genre:ficção científica,
+ageRating: 12,
+synopsis: Um astronauta enfrenta uma missão solitária de seis meses no espaço. Ele sente os efeitos extremos do isolamento e sofre com saudades da esposa. Lutando para se manter são, o homem é visitado por uma criatura alienígena que promete ajudá-lo a curar seus traumas.,
+trailer:https://youtu.be/7iilX1_L7rg?si=fzmMGZrEq0due6Nr
+},
+
+{
+title:Blade Runner 2049,
+genre:ficção científica,
+ageRating: 14,
+synopsis: Após descobrir um segredo que ameaça o que resta da sociedade, um novo policial parte em busca de Rick Deckard, que está desaparecido há 30 anos.,
+trailer:https://youtu.be/xGwe7D0RKWc?si=hM7UVxJ1pw14A5eo
+},
+
+{
+title:Chappie,
+genre:ficção científica,
+ageRating: 16,
+synopsis:Em um futuro próximo, policiais humanos são substituídos por robôs ultrarresistentes, dotados de inteligência artificial. Deon, o cientista criador das máquinas, rouba um modelo defeituoso e consegue fazer com que ele tenha a capacidade de sentir e pensar por si próprio. Porém, as autoridades começam a ver Chappie como um perigo para a humanidade e ordem e vão fazer de tudo para garantir que ele seja o último de sua espécie.,
+trailer:https://youtu.be/kxO4tUF9vN4?si=1taAUBHxH6DXeXo_
+
+============
+
+
+
+========},AÇÃO
+
+title: "Os incriveis",
+genre: "Ação",
+ageRating: livre,
+synopsis: “Em um mundo onde os super-heróis são proibidos, Bob Parr (Sr. Incrível) vive uma vida normal com sua família. Quando é chamado para uma missão secreta, ele acaba enfrentando o vilão Síndrome. Sua esposa, Helen (Mulher-Elástica), e seus filhos, que também possuem poderes, se unem para resgatá-lo e salvar o mundo. O filme mistura ação e comédia, explorando temas de família e identidade..”
+trailer: https://www.youtube.com/watch?v=iqKEnKRDkds 
+},
+{
+title: "Jurassic World",
+genre: "Ação",
+ageRating: 10,
+synopsis: "Em um parque temático de dinossauros geneticamente modificados, uma nova atração é criada para atrair visitantes. Quando o novo dinossauro, o Indominus rex, escapa e causa caos no parque, os funcionários precisam lutar para proteger os visitantes e restaurar a ordem."
+trailer: https://www.youtube.com/watch?v=aJJrkyHas78
+},
+{
+title: "Homem-Aranha: De Volta ao Lar",
+genre: "Ação",
+ageRating: 12,
+synopsis: " Peter Parker, o Homem-Aranha, precisa equilibrar sua vida como um estudante do ensino médio e como um super-herói que tenta impedir o vilão Vulture, que está vendendo armas tecnológicas.",
+trailer: https://www.youtube.com/watch?v=2x-2iYxgMFU 
+}, 
+{
+title: "Os Vingadores",
+genre: "Ação",
+ageRating: 14,
+synopsis: "Quando o vilão Loki ameaça a Terra com um exército alienígena, Nick Fury, da S.H.I.E.L.D., junta os maiores heróis do planeta para formar os Vingadores. Entre eles estão Homem de Ferro, Capitão América, Thor, Hulk, Viúva Negra e Gavião Arqueiro.",
+trailer: https://www.youtube.com/watch?v=KeNEGtsCWEk 
+},
+{
+title: "O Exterminador do Futuro 2: O Julgamento Final",
+genre: "Ação",
+ageRating: 16,
+synopsis: "Sarah Connor e seu filho John, junto a um Terminator reprogramado, lutam para impedir o apocalipse causado por Skynet. O filme mistura ação intensa com elementos de ficção científica.",
+trailer: https://www.youtube.com/watch?v=jfLN1xPh9Sw 
+},
+
+========ANIMAÇÃO!
+
+
+title: "Toy Story",
+genre: "Animação",
+ageRating: livre,
+synopsis: "Woody, um cowboy de brinquedo, vê sua liderança ameaçada por um novo brinquedo, Buzz Lightyear. Juntos, eles precisam enfrentar desafios para se tornar amigos e salvar os outros brinquedos do perigo.",
+trailer: https://www.youtube.com/watch?v=v-PjgYDrg70
+},
+{
+title: "Como Treinar o Seu Dragão",
+genre: "Animação",
+ageRating: 10,
+synopsis: "Em uma pequena vila viking, o jovem Hiccup Horrendous Haddock III, filho do chefe Stoico, é um garoto magro e desajeitado, sem as qualidades típicas de um viking. Ele sonha em ser um grande caçador de dragões, mas é constantemente subestimado pela sua comunidade. Quando ele acidentalmente captura um dragão raro e muito temido, o Fúria da Noite, ele tem a chance de provar seu valor. Porém, ao invés de matar o dragão, Hiccup faz amizade com ele, e a partir daí, começa a aprender sobre as criaturas de uma forma completamente nova, questionando tudo o que os vikings acreditavam sobre os dragões.",
+trailer: "https://www.youtube.com/watch?v=uv1V9BOb2M8&t=1s&themeRefresh=1
+"
+},
+{
+title: Kubo e a Espada Mágica
+genre: Animação
+ageRating: 12,   
+synopsis: ""Kubo e a Espada Mágica" segue a história de um jovem garoto chamado Kubo, que vive com sua mãe em uma pequena vila. Kubo tem um dom especial: ele pode contar histórias mágicas e trazer figuras de origami à vida com sua música. Quando ele acidentalmente invoca uma antiga maldição de sua família, Kubo se vê em uma jornada para encontrar a armadura mágica de seu pai, um grande guerreiro, e derrotar os espíritos vingativos que o perseguem.",
+trailer:https://www.youtube.com/watch?v=5Icq3wQEAiI	
+}
+{
+title: Jujtso Kaisen 0,
+genre: "Animação",
+ageRating: 14,
+synopsis: "Jujutsu Kaisen 0" segue Yuta Okkotsu, um jovem que é possuído por uma poderosa maldição, Rika, que foi a sua amiga de infância antes de sua morte trágica. Incapaz de controlar o poder de Rika, Yuta é levado para a Escola de Jujutsu, onde é treinado por Satoru Gojo para se tornar um feiticeiro e aprender a controlar a maldição. O filme foca na jornada de Yuta para dominar seus poderes, enquanto enfrenta perigos sobrenaturais e descobre mais sobre a verdadeira natureza da maldição que o persegue.
+trailer: https://www.youtube.com/watch?v=359noIH-wNo 
+},
+{
+title: Akira,
+genre: "Animação",
+ageRating: 16,
+synopsis: Em um futuro pós-apocalíptico, Kaneda e Tetsuo são membros de uma gangue de motociclistas em Tóquio. Após um acidente misterioso, Tetsuo desenvolve habilidades psíquicas extraordinárias, mas seu poder logo escapa de seu controle, fazendo com que ele se torne uma ameaça não apenas para seus amigos, mas para a cidade inteira. A animação é um marco no gênero, explorando temas de poder, corrupção e a luta interna com a violência.,
+trailer: https://www.youtube.com/watch?v=nA8KmHC2Z-g 
+
+
+========== 
+
+========DRAMA
+
+title: Cartas para Deus,
+genre: drama,
+ageRating: livre,
+synopsis:Um menino de apenas oito anos de idade tem câncer e escreve suas orações em cartas para seu amigo Deus. Buscando um sentido para a vida, um carteiro encontra as orações do garoto e resolve ajudá-lo a mudar o destino de todos ao seu redor.,
+trailer:https://youtu.be/KUO4vGVSlkc?si=GyB3zwh_0UyyVWhV
+
+},
+
+{
+title:Meninas Não Choram,
+genre:drama,
+ageRating: 10,
+synopsis:Pipa, 16 anos, é fã de futebol feminino e o destaque do time da escola. Popular e com um futuro promissor, ela vê os seus planos interrompidos com o diagnóstico de leucemia e uma internação. Mas a jornada no hospital é marcada pela amizade de Beca.,
+trailer:https://youtu.be/88YilPjdCAY?si=l0EmSIRCmwuQXL65
+},
+
+{
+title:O Menino do Pijama Listrado,
+genre:drama,
+ageRating: 12,
+synopsis: Durante a Segunda Guerra Mundial, Bruno, um garoto de oito anos, e sua família saem de Berlim para residir próximo a um campo de concentração, onde seu pai acaba de se tornar comandante. Infeliz e solitário, ele vagueia fora de sua casa e certo dia encontra Shmuel, um menino judeu de sua idade. Embora a cerca de arame farpado do campo os separem, os meninos começam uma amizade proibida.,
+trailer:https://youtu.be/9U4NDpUw8dA?si=Pl068SrqDlTlkW6O
+},
+
+{
+title:A Sociedade da Neve,
+genre:drama,
+ageRating: 14,
+synopsis:Em 1972, um voo vindo do Uruguai colide com uma geleira nos Andes. Apenas 29 dos seus 45 passageiros sobreviveram ao acidente. Presos em um dos ambientes mais hostis do planeta, eles são forçados a lutar pelas suas vidas.,
+trailer:https://youtu.be/9cjSePIf1l0?si=OdoX-u6OQG1zC63V
+},
+
+{
+title:Animais Noturnos,
+genre:drama,
+ageRating: 16,
+synopsis: Um escritor pede a sua ex-mulher para ler o manuscrito de seu novo romance, uma história sobre um homem de família cuja vida dá uma guinada sombria.,
+trailer:https://youtu.be/Wmgq_XA1iBM?si=o0q4vd6yy6WQZn1x
+
+========TERROR
+ title:A Noiva-Cadáver, 
+genre:terror,
+ageRating: livre,
+synopsis:As famílias de Victor e Victoria estão arranjando seu casamento. Nervoso com a cerimônia, Victor vai sozinho à floresta para ensaiar seus votos. No entanto, o que ele pensava ser um tronco de árvore na verdade é o braço esquelético de Emily, uma noiva que foi assassinada depois de fugir com seu amor. Convencida que Victor acabara de lhe pedir a mão em casamento, Emily o leva para o mundo dos mortos, mas ele precisa retornar rapidamente antes que Victoria se case com o malvado Lorde Barkis.,
+trailer:https://youtu.be/4hGY0B7XjBk?feature=shared
+},
+
+{
+title: A Casa Monstro,
+genre: terror,
+ageRating: 10,
+synopsis: Nenhum adulto acredita quando três adolescentes falam que existe uma casa no bairro que é uma criatura perigosa. Com o Dia das Bruxas se aproximando, eles têm que descobrir uma forma de destruir a casa antes que ela faça mal a crianças inocentes.,
+trailer:https://youtu.be/4Qb-hHqwXJA?si=2AmImreGwRUBk0-i
+},
+
+{
+title:Christine, o Carro Assassino,
+genre:terror,
+ageRating: 12,
+synopsis:Arnie Cunningham compra um Plymouth Fury 1958, que ele chama de Christine. Ele então desenvolve uma obsessão doentia pelo carro, para o desespero de seu amigo atleta, Dennis Guilder. Depois do valentão Buddy Repperton desfigurar Christine, o carro se restaura perfeitamente e começa a matar Buddy e seus amigos. Determinado a acabar com as mortes, Dennis e a namorada de Arnie, Leigh Cabot, decidem destruir Christine.,
+trailer:https://youtu.be/rPyfweSPCtE?si=Bwa8j8vLMx0v1J0R
+},
+
+{
+title:Ouija 2,
+genre:terror,
+ageRating: 14,
+synopsis:Doris é uma menina solitária e sua mãe finge se comunicar com espíritos. Certo dia, a garota usa um tabuleiro de Ouija para contato com o falecido pai e uma série de seres malignos se apoderam de seu corpo.,
+trailer:https://youtu.be/EqO-Az95qRo?si=kxE90g9NYkmsJ-_q
+},
+
+{
+title:Rua do Medo: 1994 - Parte 1,
+genre:terror,
+ageRating: 16,
+synopsis:Após uma série de assassinatos brutais, um grupo de adolescentes enfrenta uma força maligna que aterroriza uma cidade há séculos.,
+trailer:https://youtu.be/3jiRtYuunZw?si=GwZoaEByw9Kz7Dh4
+
+=========
+
+========SUSPENSE 
+
+title:Scooby-Doo! E a Maldição do Monstro do Lago'’,
+genre:suspense,
+ageRating:livre,
+synopsis:A turma da Mistérios S.A. vai até o clube do tio de Daphne para arranjar empregos temporários de verão. Enquanto estão envolvidos em suas tarefas, os amigos presenciam estranhos acontecimentos no local.,
+trailer:https://youtu.be/8HcTPTThQHc?si=3xKf0yTyQP1xJriS
+},
+{
+title:Coraline e o Mundo Secreto,
+genre:suspense,
+ageRating: 10,
+synopsis:Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente nesse mundo paralelo.,
+trailer:https://youtu.be/P8lfdvXkhu0?feature=shared
+},
+{
+title:Nerve - Um Jogo Sem Regras,
+genre:suspense,
+ageRating: 12,
+synopsis:A estudante Vee, pressionada pelos amigos, decide participar do jogo online Nerve, que faz desafios reais aos seus jogadores. Porém, o jogo toma um rumo assustador e, ao chegar no estágio final, Vee precisa tomar decisões que irão determinar o seu futuro.,
+trailer:https://youtu.be/NGKDZKqtoKw?si=-XOhqllYOoRlKhs9
+},
+{
+title:Sangue frio,
+genre:suspense,
+ageRating: 14,
+synopsis:Um homem fica preso enquanto dirigia pelas Montanhas Rochosas do Colorado durante uma nevasca perigosa. Sem poder sair de seu carro, ele logo se vê em uma luta pela sobrevivência contra o frio mortal e uma misteriosa fera que o persegue.,
+trailer:https://youtu.be/QVukC5BghDM?si=xYVM5Qu3FLTyhQCo
+},
+{
+title:O Assassino de Clovehitch,
+genre:suspense,
+ageRating: 16,
+synopsis:Um adolescente descobre provas que mostram que seu pai, um homem aparentemente calmo e bondoso, é um serial killer. O rapaz começa a investigar o caso e se vê dividido ao tentar entender se o homem que o criou é um assassino cruel.,
+trailer:https://youtu.be/aJO3G17JqZ8?si=-N8riMHm-rTTUW_S
+},
+
+========AVENTURA
+
+title:Moana - Um Mar de Aventuras,
+genre:aventura,
+ageRating:livre,
+synopsis:Uma jovem parte em uma missão para salvar seu povo. Durante a jornada, Moana conhece o outrora poderoso semideus Maui, que a guia em sua busca para se tornar uma mestre em encontrar caminhos. Juntos, eles navegam pelo oceano em uma viagem incrível.,
+trailer:https://youtu.be/mUVUmG9vOFw?feature=shared
+},
+{
+title:Jack, o Caçador de Gigantes,
+genre:aventura,
+ageRating: 10,
+synopsis:Jack é um pobre fazendeiro que abre um portal entre o mundo real e o universo de uma terrível raça de gigantes, desencadeando uma guerra. A situação obriga Jack a lutar para deter os gigantes e salvar a raça humana.,
+trailer:https://youtu.be/Z121wFs-Oe0?si=e8AHRvzrUg_6_pRK
+},
+{
+title:Jogador Nº 1,
+genre:aventura,
+ageRating: 12,
+synopsis:Em 2045, Wade Watts, assim como o resto da humanidade, prefere a realidade virtual do jogo OASIS ao mundo real. James Halliday, o excêntrico criador do jogo, morre e deixa sua fortuna inestimável para a primeira pessoa que descobrir a chave de um quebra-cabeça diabólico que ele arquitetou. Para vencer, Watts precisa abandonar a existência virtual e experimentar o amor e a realidade.,
+trailer:https://youtu.be/q_1OJNcTld0?si=TaJtBJX94ik5xYtN
+},
+{
+title:Na Selva,
+genre:aventura,
+ageRating: 14,
+synopsis:Um misterioso guia acompanha um aventureiro e seu amigo na floresta amazônica. A jornada se torna aterrorizante à medida que os elementos mais sombrios da natureza humana e as mais mortíferas ameaças os forçam a lutar pela sobrevivência.,
+trailer:https://youtu.be/vQaLraAr8OY?si=O5HtM4FN2fgXJHKn
+},
+{
+title:A Estrada,
+genre:aventura,
+ageRating: 16,
+synopsis:A América é uma sombra sinistra e cinzenta de si mesma após uma catástrofe. Um homem e seu filho vagam por este mundo pós-apocalíptico, tentando manter vivo o sonho da civilização. Eles viajam em direção ao mar sobrevivendo da melhor maneira possível, tentando evitar gangues de humanos selvagens que querem transformá-los em escravos, ou pior.,
+trailer:https://youtu.be/twtlIDR6AzI?si=yCuW8eBGH1ODFNV4
+},
+
